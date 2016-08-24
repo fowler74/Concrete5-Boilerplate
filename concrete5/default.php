dc2e116079bf8a4fbbf7d83484e3e1e5122358a0
John <fowler74@icloud.com>
2016-08-24 11:25:54 -0400

    Uploaded Concrete5 Template

---
+++ concrete5/default.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php Loader::element('header_required') ?>
+		<!-- Place additional <head> information below -->	
+
+	</head>
+	<body>
+		<div class="<?= $c->getPageWrapperClass() ?>">
+		<!-- Begin custom html -->
+			
+
+
+		<!-- End custom html -->
+		</div>
+		<?php Loader::element('footer_required') ?>
+	</body>
+</html>
